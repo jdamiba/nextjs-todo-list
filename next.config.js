8eca286c5f8d1ebc58c4ba96438b8108a4d3a68e
Joseph Damiba <josephdamiba@Josephs-MacBook-Pro.local>
2024-03-19 13:55:43 -0700

    add firebase keys

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    FIREBASE_APP_API_KEY: 'AIzaSyCdQmpMSFYCJGTSngbIg5TQMcvxMzPU_8E',
-    FIREBASE_APP_AUTH_DOMAIN: 'todo-list-challenge-f1876.firebaseapp.com',
+    FIREBASE_APP_API_KEY: 'AIzaSyCem4YCROFYnvBl0mjHiquQ_Wl66hy0Qsg',
+    FIREBASE_APP_AUTH_DOMAIN: 'to-do-list-b1856.firebaseapp.com',
     FIREBASE_APP_DATABASE_URL: 'https://todo-list-challenge-f1876.firebaseio.com',
-    FIREBASE_APP_PROJECT_ID: 'todo-list-challenge-f1876',
-    FIREBASE_APP_STORAGE_BUCKET: 'todo-list-challenge-f1876.appspot.com',
-    FIREBASE_APP_MESSAGING_SENDER_ID: '273217147434',
-    FIREBASE_APP_ID: '1:273217147434:web:8599ae8887f4c66620e953',
-    FIREBASE_MEASUREMENT_ID: 'G-LXB6MZ5YCN',
+    FIREBASE_APP_PROJECT_ID: 'to-do-list-b1856',
+    FIREBASE_APP_STORAGE_BUCKET: 'to-do-list-b1856.appspot.com',
+    FIREBASE_APP_MESSAGING_SENDER_ID: '781968947808',
+    FIREBASE_APP_ID: '1:781968947808:web:1d974e0c4ceecf4b372105',
+    FIREBASE_MEASUREMENT_ID: 'G-X8KKM0KXJ9',
   },
 };
